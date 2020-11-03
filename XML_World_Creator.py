fff51c9b00ae10e1ce08912d00a8a84762a4da83
Rohan Mannem <rmannem5@gmail.com>
2020-11-03 10:25:54 -0800

    Update XML_World_Creator.py
    many world changes (flowers, walls, torches, and glowstone)

--- XML_World_Creator.py
+++ XML_World_Creator.py
@@ -4,12 +4,58 @@ from malmo import MalmoPython
 import os
 import sys
 import time
+import random
 
 if sys.version_info[0] == 2:
     sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 else:
     import functools
     print = functools.partial(print, flush=True)
+
+# HYPERPARAMETERS
+flowerChance = 0.01
+SIZE = 21
+waterGap = SIZE/5
+
+flowerXML = ""
+for x in range (-SIZE + 4, SIZE - 3):
+  for z in range (-SIZE + 4, SIZE - 3):
+    plant = random.uniform(0, 1)
+    if plant <= flowerChance:
+      type = random.randint(0, 1)
+      if type <= 0.5:
+        flowerXML += "<DrawBlock x='{}' y='{}' z='{}' type='red_flower'/>".format(x, 227, z)
+      else:
+        flowerXML += "<DrawBlock x='{}' y='{}' z='{}' type='yellow_flower'/>".format(x, 227, z)
+
+floorLightsXML = ""
+for x in range (-SIZE, SIZE):
+  for z in range (-SIZE, SIZE):
+    if x % 4 == 0 and z % 4 == 0 and x != 0 and z != 0 and (x == z or x == -z):
+      floorLightsXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glowstone'/>".format(x, 226, z, (-1 * x), 226, z)
+      floorLightsXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glowstone'/>".format((-1 * x), 226, z, (-1 * x), 226, (-1 * z))
+      floorLightsXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glowstone'/>".format((-1 * x), 226, (-1 * z), x, 226, (-1 * z))
+      floorLightsXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glowstone'/>".format(x, 226, (-1 * z), x, 226, z)
+floorLightsXML += "<DrawBlock x='{}' y='{}' z='{}' type='glowstone'/>".format(0, 226, 0)
+
+waterXML = ""
+for x in range (-SIZE, SIZE):
+  for z in range (-SIZE, SIZE):
+    if (-SIZE + waterGap < x < -SIZE or SIZE - waterGap < x < SIZE) and ((-SIZE + waterGap < z < -SIZE or SIZE - waterGap < z < SIZE)) and (x == z or x == -z):
+      waterXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='water'/>".format(x, 226, z, (-1 * x), 226, z)
+      waterXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='water'/>".format((-1 * x), 226, z, (-1 * x), 226, (-1 * z))
+      waterXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='water'/>".format((-1 * x), 226, (-1 * z), x, 226, (-1 * z))
+      waterXML += "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='water'/>".format(x, 226, (-1 * z), x, 226, z)
+
+torchXML = ""
+for x in range (-SIZE + 1, SIZE):
+  if x % 2 == 0:
+    torchXML += "<DrawBlock x='{}' y='{}' z='{}' type='torch'/>".format(x, 233, SIZE)
+    torchXML += "<DrawBlock x='{}' y='{}' z='{}' type='torch'/>".format(x, 233, -SIZE)
+for z in range (-SIZE + 1, SIZE):
+  if z % 2 == 0:
+    torchXML += "<DrawBlock x='{}' y='{}' z='{}' type='torch'/>".format(SIZE, 233, z)
+    torchXML += "<DrawBlock x='{}' y='{}' z='{}' type='torch'/>".format(-SIZE, 233, z)
 
 missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
             <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -21,30 +67,39 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
               <ServerSection>
                 <ServerInitialConditions>
                   <Time>
-                    <StartTime>6000</StartTime>
-                    <AllowPassageOfTime>false</AllowPassageOfTime>
+                    <StartTime>18000</StartTime>
                   </Time>
                   <Weather>clear</Weather>
                   <AllowSpawning>true</AllowSpawning>
                 </ServerInitialConditions>
                 <ServerHandlers>
-                  <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1"/>
-                  <DrawingDecorator>
-                    <DrawEntity x="0"  y="227" z="8" type="Pig"/>
-                    <DrawEntity x="3"  y="227" z="8" type="Cow"/>
-                    <DrawEntity x="-3"  y="227" z="8" type="Chicken"/>
-                    <DrawEntity x="6"  y="227" z="8" type="Sheep"/>
+                  <FlatWorldGenerator generatorString="3;7,220*1,5*3,2;3;,biome_1" forceReset="true"/>
+                  <DrawingDecorator>''' + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='brick_block'/>".format(-SIZE, 227, -SIZE, SIZE, 233, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='brick_block'/>".format(-SIZE, 227, -SIZE, -SIZE, 233, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='brick_block'/>".format(SIZE, 227, SIZE, SIZE, 233, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='brick_block'/>".format(SIZE, 227, SIZE, -SIZE, 233, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(-SIZE, 228, -SIZE, SIZE, 228, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(-SIZE, 228, -SIZE, -SIZE, 228, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(SIZE, 228, SIZE, SIZE, 228, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(SIZE, 228, SIZE, -SIZE, 228, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='iron_block'/>".format(-SIZE, 230, -SIZE, SIZE, 230, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='iron_block'/>".format(-SIZE, 230, -SIZE, -SIZE, 230, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='iron_block'/>".format(SIZE, 230, SIZE, SIZE, 230, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='iron_block'/>".format(SIZE, 230, SIZE, -SIZE, 230, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(-SIZE, 232, -SIZE, SIZE, 232, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(-SIZE, 232, -SIZE, -SIZE, 232, SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(SIZE, 232, SIZE, SIZE, 232, -SIZE) + \
+                    "<DrawCuboid x1='{}' y1='{}' z1='{}' x2='{}' y2='{}' z2='{}' type='glass'/>".format(SIZE, 232, SIZE, -SIZE, 232, SIZE) + \
+                    floorLightsXML + \
+                    flowerXML + \
+                    waterXML + \
+                    torchXML + \
+                    '''<DrawEntity x="0" y="227" z="8" type="Pig"/>
+                    <DrawEntity x="5" y="227" z="8" type="Cow"/>
+                    <DrawEntity x="-5" y="227" z="8" type="Chicken"/>
+                    <DrawEntity x="10" y="227" z="8" type="Sheep"/>
 
-                    <DrawCuboid x1="-10" y1="227" z1="-10" x2="10" y2="227" z2="-10" type="fence"/>
-                    <DrawCuboid x1="-10" y1="227" z1="-10" x2="-10" y2="227" z2="10" type="fence"/>
-                    <DrawCuboid x1="10" y1="227" z1="10" x2="10" y2="227" z2="-10" type="fence"/>
-
-                    <DrawCuboid x1="-9" y1="226" z1="10" x2="9" y2="226" z2="10" type="water"/>
-                    <DrawCuboid x1="9" y1="226" z1="10" x2="9" y2="226" z2="20" type="water"/>
-                    <DrawCuboid x1="9" y1="226" z1="20" x2="-9" y2="226" z2="20" type="water"/>
-                    <DrawCuboid x1="-9" y1="226" z1="20" x2="-9" y2="226" z2="10" type="water"/>
-
-                    <DrawBlock x="-5"  y="226" z="0" type="lava" />
                   </DrawingDecorator>
                   <ServerQuitFromTimeUp timeLimitMs="1000000"/>
                   <ServerQuitWhenAnyAgentFinishes/>
@@ -59,10 +114,9 @@ missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
                     <InventoryItem slot="0" type="diamond_sword"/>
                     <InventoryItem slot="1" type="diamond_pickaxe"/>
                     <InventoryItem slot="2" type="fishing_rod"/>
-                    <InventoryItem slot="3" type="bone" quantity="64"/>
-                    <InventoryItem slot="4" type="wheat" quantity="64"/>
-                    <InventoryItem slot="5" type="apple" quantity="64"/>
-                    <InventoryItem slot="6" type="saddle" quantity="64"/>
+                    <InventoryItem slot="3" type="wheat" quantity="64"/>
+                    <InventoryItem slot="4" type="carrot" quantity="64"/>
+                    <InventoryItem slot="5" type="melon_seeds" quantity="64"/>
                 </Inventory>
                 </AgentStart>
                 <AgentHandlers>
